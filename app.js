89bac5c18005b9b327b5e7d75a3c55559251ae38
userwijnja <56116163+userwijnja@users.noreply.github.com>
2023-12-20 10:37:16 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+<script>
+document.getElementById('haarlem').onclick = function() {
+  document.getElementById('adres').innerText = 'adresvoorbeeld1' + '\n' +
+  '1234ah Uithoorn' + '\n' + '\n';
+}
+document.getElementById('uithoorn').onclick = function() {
+  document.getElementById('adres').innerText = 'adresvoorbeeld2' + '\n' +  
+  '1234ah Haarlem' + '\n' + '\n';
+}
+</script>
